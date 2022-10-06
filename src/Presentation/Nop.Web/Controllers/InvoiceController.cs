68df0e4dd7cad459163d310fa513d7ba30f55d52
Omid Kheiri <omidkheiri@gmail.com>
2022-10-06 14:44:59 +0300

    Add project files.

---
+++ src/Presentation/Nop.Web/Controllers/InvoiceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nop.Web.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
